2cfcde4d449ca822ab6a078b672132492814f2ca
rashhmi1 <rashmi22netam@gmail.com>
2026-08-15 16:58:45 +0530

    Restore portal menu source file

---
+++ src/portal_menu.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+void displayMenu() {
+    std::cout << "\n--- Student Portal Menu ---" << std::endl;
+    std::cout << "1. View Student Information" << std::endl;
+    std::cout << "2. View Academic Result" << std::endl;
+    std::cout << "3. Exit" << std::endl;
+}
