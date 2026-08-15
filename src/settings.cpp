299c3988790aaba3f8cadea4814f0937c766084f
rashhmi1 <rashmi22netam@gmail.com>
2026-08-15 21:47:33 +0530

    Add student portal settings display

---
+++ src/settings.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+void displaySettings() {
+    std::cout << "\n--- Student Portal Settings ---" << std::endl;
+    std::cout << "1. Notifications" << std::endl;
+    std::cout << "2. Privacy" << std::endl;
+    std::cout << "3. Account" << std::endl;
+}
