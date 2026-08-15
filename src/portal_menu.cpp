dee9d5a1ec2040487a53a1483c9c555127aea11c
rashhmi1 <rashmi22netam@gmail.com>
2026-08-15 16:45:14 +0530

    Remove portal menu source

--- src/portal_menu.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-void displayMenu() {
-    std::cout << "\n--- Student Portal Menu ---" << std::endl;
-    std::cout << "1. View Student Information" << std::endl;
-    std::cout << "2. View Academic Result" << std::endl;
-    std::cout << "3. Exit" << std::endl;
-}
